580cc49ff274010e6d7a1d06c15c04d90d7a9da4
LeKohrs <leichliter.ryan@gmail.com>
2018-04-20 16:55:28 -0500

    added info and images. Added firebase

--- app.js
+++ app.js
@@ -1,30 +1,118 @@
-window.onload = function() {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD-ayFxsfLD0_PrBZht9Z93iWWpSdgahOE",
+    authDomain: "spindrift-4f1b2.firebaseapp.com",
+    databaseURL: "https://spindrift-4f1b2.firebaseio.com",
+    projectId: "spindrift-4f1b2",
+    storageBucket: "",
+    messagingSenderId: "505378437671"
+};
+firebase.initializeApp(config);
 
-}
+const database = firebase.database()
 
-function keyPress() {
-    window.addEventListener("keyup", function(e) {
-        switch (e.key) {
-            case 'w':
-                fruit.type = 'apple';
-                this.keysPressed.push(fruit);
-                this.sendLightsToFirebase('red');
-                break;
-            case 'e':
-                fruit.type = 'banana';
-                this.keysPressed.push(fruit);
-                this.sendLightsToFirebase('yellow');
-                break;
-            case 'r':
-                fruit.type = 'celery';
-                this.keysPressed.push(fruit);
-                this.sendLightsToFirebase('green');
-                break;
-            case 't':
-                fruit.type = 'blueberry';
-                this.keysPressed.push(fruit);
-                this.sendLightsToFirebase('blue');
-                break;
+var app = new Vue({
+    el: '#app',
+    data: {
+        drinks: [],
+        drink: {},
+        showDrink: false,
+        showMain: true
+    },
+    methods: {
+        keyPress: function() {
+            window.addEventListener("keyup", e => {
+                this.showDrink = true;
+                this.showMain = false;
+                switch (e.key) {
+                    case 'w':
+                        if(this.drink == this.drinks.HalfHalf.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.HalfHalf.drink;
+                        }
+                        break;
+                    case 'e':
+                        if(this.drink == this.drinks.Strawberry.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.Strawberry.drink;
+                        }
+                        break;
+                    case 'r':
+                        if(this.drink == this.drinks.Grapefruit.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.Grapefruit.drink;
+                        }
+                        break;
+                    case 't':
+                        if(this.drink == this.drinks.Blackberry.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.Blackberry.drink;
+                        }
+                        break;
+                    case 'y':
+                        if(this.drink == this.drinks.Cucumber.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.Cucumber.drink;
+                        }
+                        break;
+                    case 'u':
+                        if(this.drink == this.drinks.Lemon.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.Lemon.drink;
+                        }
+                        break;
+                    case 'i':
+                        if(this.drink == this.drinks.OrangeMango.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.OrangeMango.drink;
+                        }
+                        break;
+                    case 'o':
+                        if(this.drink == this.drinks.RaspberryLime.drink) {
+                            this.reset();
+                        }
+                        else {
+                            this.drink = {};
+                            this.drink = this.drinks.RaspberryLime.drink;
+                        }
+                        break;
+                }
+            });
+        },
+        reset: function() {
+            this.showDrink = false;
+            this.showMain = true;
+            this.drink = {};
         }
-    });
-}
+    },
+    mounted() {
+        this.keyPress();
+    },
+    created() {
+        database.ref('drinks').on('value', snapshot => {
+            this.drinks = snapshot.val();
+        })
+        // console.log(this.drinks);
+    }
+})
