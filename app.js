7d3d45d7be11ef9d593daee1bc3635437126c817
Ryan Leichliter <leichliter.ryan@gmail.com>
2018-05-07 16:56:28 -0500

    Added audio and reaction to somone holding the can.

--- app.js
+++ app.js
@@ -17,93 +17,96 @@ var app = new Vue({
         drinks: [],
         drink: {},
         showDrink: false,
-        showMain: true
+        showMain: true,
+        keyTick: 0,
+        firstWarning: false,
+        finalWarning: false,
+        alarm: false,
+        shutdown: false
     },
     methods: {
         keyPress: function() {
+            window.addEventListener("keydown", e => {
+                if(e.key === 'w' || e.key === 'e' || e.key === 'r' || e.key === 't' || e.key === 'y' || e.key === 'u' || e.key === 'u' || e.key === 'i' || e.key === 'o') {
+                    console.log(e.key)
+                    this.keyTick++
+                    if(this.keyTick >= 20 && this.keyTick <= 40) {
+                        this.firstWarning = true
+                        console.log('get a room')
+                    }
+                    else if (this.keyTick >= 41 && this.keyTick <= 60) {
+                        this.firstWarning = false
+                        this.finalWarning = true
+                        console.log('Let go of the can')
+                    }
+                    else if (this.keyTick >= 61 && this.keyTick <= 100) {
+                        this.finalWarning = false
+                        this.alarm = true
+                        let alarm = document.getElementsByClassName('alarm-bell')[0]
+                        alarm.play()
+                        console.log('alarm bells')                        
+                    }
+                    else if (this.keyTick >= 101) {
+                        this.alarm = false
+                        let alarm = document.getElementsByClassName('alarm-bell')[0]
+                        alarm.pause()
+                        alarm.currentTime = 0
+                        this.shutdown = true
+                        console.log("You're done. Move along")                        
+                    }
+                }   
+            });
             window.addEventListener("keyup", e => {
-                this.showDrink = true;
-                this.showMain = false;
+                this.showDrink = true
+                this.showMain = false
+                if(e.keyCode === 32){
+                    this.reset()
+                }
                 switch (e.key) {
                     case 'w':
-                        if(this.drink == this.drinks.HalfHalf.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.HalfHalf.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.HalfHalf.drink;
                         break;
                     case 'e':
-                        if(this.drink == this.drinks.Strawberry.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.Strawberry.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.Strawberry.drink;
                         break;
                     case 'r':
-                        if(this.drink == this.drinks.Grapefruit.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.Grapefruit.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.Grapefruit.drink;
                         break;
                     case 't':
-                        if(this.drink == this.drinks.Blackberry.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.Blackberry.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.Blackberry.drink;
                         break;
                     case 'y':
-                        if(this.drink == this.drinks.Cucumber.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.Cucumber.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.Cucumber.drink;
                         break;
                     case 'u':
-                        if(this.drink == this.drinks.Lemon.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.Lemon.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.Lemon.drink;
                         break;
                     case 'i':
-                        if(this.drink == this.drinks.OrangeMango.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.OrangeMango.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.OrangeMango.drink;
                         break;
                     case 'o':
-                        if(this.drink == this.drinks.RaspberryLime.drink) {
-                            this.reset();
-                        }
-                        else {
-                            this.drink = {};
-                            this.drink = this.drinks.RaspberryLime.drink;
-                        }
+                        this.drink = {};
+                        this.drink = this.drinks.RaspberryLime.drink;
                         break;
                 }
             });
         },
         reset: function() {
+            this.firstWarning = false
+            this.finalWarning = false
+            this.alarm = false
+            this.shutdown = false            
             this.showDrink = false;
             this.showMain = true;
             this.drink = {};
+            this.keyTick = 0;
         }
     },
     mounted() {
